d6c85ad1e9ca2aef6220f7b8328a2a0fa0546728
Dari <72017271+Pluzhnikovadari@users.noreply.github.com>
2021-11-14 15:59:21 +0300

    Update example_transfer.rs

--- substrate-api-client/src/examples/example_transfer.rs
+++ substrate-api-client/src/examples/example_transfer.rs
@@ -2,41 +2,75 @@ use clap::{load_yaml, App};
 use keyring::AccountKeyring;
 use sp_core::crypto::Pair;
 use sp_runtime::MultiAddress;
+use std::env;
 
 use substrate_api_client::{Api, XtStatus};
 
+
 fn main() {
+    let mut acc = String::new();
+    let line = std::io::stdin().read_line(&mut acc).unwrap();
     env_logger::init();
     let url = get_node_url_from_cli();
 
-    let from = AccountKeyring::Alice.pair();
+    let mut from = AccountKeyring::Alice.pair();
+    let mut fr = AccountKeyring::Alice.to_account_id();
+
+
+    if acc == String::from("Alice\n") {
+        from = AccountKeyring::Alice.pair();
+        fr = AccountKeyring::Alice.to_account_id();
+    } else if acc == String::from("Bob\n") {
+        from = AccountKeyring::Bob.pair();
+        fr = AccountKeyring::Bob.to_account_id();
+    } else if acc == String::from("Charlie\n") {
+        from = AccountKeyring::Charlie.pair();
+        fr = AccountKeyring::Charlie.to_account_id();
+    } else if acc == String::from("Dave\n") {
+        from = AccountKeyring::Dave.pair();
+        fr = AccountKeyring::Dave.to_account_id();
+    } else if acc == String::from("Eve\n") {
+        from = AccountKeyring::Eve.pair();
+        fr = AccountKeyring::Eve.to_account_id();
+    } else if acc == String::from("Ferdie\n") {
+        from = AccountKeyring::Ferdie.pair();
+        fr = AccountKeyring::Ferdie.to_account_id();
+    }
+
+
     let api = Api::new(url)
         .map(|api| api.set_signer(from.clone()))
         .unwrap();
 
-    let fr = AccountKeyring::Alice.to_account_id();
-    
-    match api.get_account_data(&fr).unwrap() {
-        Some(alice) => println!("[+] Alice's Free Balance is is {}\n", alice.free),
-        None => println!("[+] Alice's Free Balance is is 0\n"),
-    }
 
+    let balance = api.get_account_data(&fr).unwrap().unwrap();
+    println!("[+] Free Balance is {}\n", balance.free);
     
+    /*
+    match api.get_account_data(&fr).unwrap() {
+        Some(alice) => println!("[+] Free Balance is is {}\n", alice.free),
+        None => println!("[+] Free Balance is is 0\n"),
+    }
+    */
+
+    let diff: u128 = 42;
+
     let xt = api.balance_set_balance(
         MultiAddress::Id(fr.clone()),
-        42,
+        42 - diff,
         42
     );
 
-    println!("[+] Composed extrinsic: {:?}\n", xt);
 
+    // send and watch extrinsic until finalized
     let tx_hash = api
         .send_extrinsic(xt.hex_encode(), XtStatus::InBlock)
         .unwrap();
     println!("[+] Transaction got included. Hash: {:?}\n", tx_hash);
     
+
     let alice = api.get_account_data(&fr).unwrap().unwrap();
-    println!("[+] Alice's Free Balance is now {}\n", alice.free);
+    println!("[+] Free Balance is now {}\n", alice.free);
     
 }
 
