41cc53de871ea850a6018d67ddb819b5839c5a14
Dari <72017271+Pluzhnikovadari@users.noreply.github.com>
2021-11-22 20:17:06 +0300

    Update punish.py

--- punish.py
+++ punish.py
@@ -8,14 +8,50 @@ def run_command(cmd, output="print", exit_on_error=False):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, shell=True,
                          universal_newlines=True)
-    p.communicate()
+    o, e = p.communicate()
 
 
+def to_string(ip_path):
+    ip_path = ip_path.split('.')
+    res = "1" + "0" * (3 - len(ip_path[0])) + ip_path[0] + \
+          "0" * (3 - len(ip_path[1])) + ip_path[1] + \
+          "0" * (3 - len(ip_path[2])) + ip_path[2]
+    if "/" in ip_path:
+        ip_path = ip_path[-1].split('/')
+        res += "0" * (3 - len(ip_path[0])) + ip_path[0] + ip_path[-1]
+    else:
+        res += "0" * (3 - len(ip_path[3])) + ip_path[3] + '24'
+    return res
 
-if __name__ == "__main__":
 
+def bob_func():
+    print("in")
+    event_get = open("substrate-api-client_1/events.txt", "r")
+    address = ''
+    cnt = 0
+    while True:
+        # работа с событиями и добавление пути в quagga.
+        address = address + event_get.readline()
+        if address and address[-1] == '\n':
+            print("got")
+            address = address.split(", ")[-1][:-2]
+            with open('/etc/quagga/bgpd.conf', "r+") as f:
+                line = f.readlines()
+            delit = str(int(address[1:4])) + "." + str(int(address[4:7])) + \
+                "." + str(int(address[7:10])) + "." + \
+                str(int(address[10:13]))
+            
+            map_neigh = ""
+        
+            for i in range(len(line)):
+                if (delit in line[i]):
+                    line = line[:i] + line[i + 1:]
+                    break
+            with open('/etc/quagga/bgpd.conf', "w+") as f:
+                for el in line:
+                    f.write(el)
     
-    
+def alice_fun():
     path = input("Write path: ")
     acc = input("Write acc: ")
 
@@ -40,7 +76,12 @@ if __name__ == "__main__":
                          universal_newlines=True)
     
 
-    with open('substrate-api-client_1/events.txt',
-        'w') as send_data_write:
-        send_data_write.write("1169254253000024\n")
-    
+  if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print("Add description for mode")
+    else:
+        if sys.argv[1] == "bob":
+            bob_func()
+        elif sys.argv[1] == "alice":
+            alice_func()
